4aa51db0af3c883ba7df600e11fc514e31d579e2
Stephen Suffian <stephen.suffian@gmail.com>
2020-08-09 12:25:54 -0400

    gh-pages

---
+++ precache-manifest.9e285a4b5124422675ed1ed4995c79dc.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/redo-safari/img/brand/blue.png"
+  },
+  {
+    "revision": "e723e92e6917f683cca29ab962944f56",
+    "url": "/redo-safari/img/slider/zebras.jpg"
+  },
+  {
+    "revision": "e6a5d50cd294796f289c",
+    "url": "/redo-safari/js/chunk-52e4de86.0b9689e0.js"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/redo-safari/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "7f366453a324271df33c",
+    "url": "/redo-safari/js/chunk-ebdaf33a.640c6a80.js"
+  },
+  {
+    "revision": "bef7bc00dcfce64dd9f7",
+    "url": "/redo-safari/js/chunk-vendors.e6836401.js"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/redo-safari/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/redo-safari/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/redo-safari/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/redo-safari/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/redo-safari/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/redo-safari/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/redo-safari/img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/redo-safari/img/theme/promo-1.png"
+  },
+  {
+    "revision": "cbb8ee68ef8445b68751",
+    "url": "/redo-safari/js/app.a4e5e6b2.js"
+  },
+  {
+    "revision": "46ed96e88e7ce819d4a5b5f538b2f2cf",
+    "url": "/redo-safari/index.html"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/redo-safari/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/redo-safari/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "279455192256df3a2d975a2954a39d6e",
+    "url": "/redo-safari/img/slider/lion.jpg"
+  },
+  {
+    "revision": "3edf271e47aa32c91a8a4edd619ce6ef",
+    "url": "/redo-safari/img/slider/elephants.jpg"
+  },
+  {
+    "revision": "dc07c81b465b1dee8a45873d2d93571f",
+    "url": "/redo-safari/img/slider/elephant.jpg"
+  },
+  {
+    "revision": "5307ff7924ed2205ca59d0144ed8e979",
+    "url": "/redo-safari/img/slider/buffalo.jpg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/redo-safari/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/redo-safari/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/redo-safari/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/redo-safari/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/redo-safari/img/brand/white.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/redo-safari/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/redo-safari/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/redo-safari/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/redo-safari/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/redo-safari/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/redo-safari/img/brand/favicon.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/redo-safari/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/redo-safari/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/redo-safari/favicon.png"
+  },
+  {
+    "revision": "7f366453a324271df33c",
+    "url": "/redo-safari/css/chunk-ebdaf33a.79c177b7.css"
+  },
+  {
+    "revision": "cbb8ee68ef8445b68751",
+    "url": "/redo-safari/css/app.47ea53ed.css"
+  }
+];
